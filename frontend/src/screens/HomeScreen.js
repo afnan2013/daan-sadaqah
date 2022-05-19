55fa828f4ff7c659e348985cf7391f530b7eab51
kucse160235 <afnan1635@cseku.ac.bd>
2022-05-20 00:27:39 +0600

    Converting Functional Comp. to Class Comp
    1. Homepage is done
    2. LoginPage is done

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,56 +1,66 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Button, Image } from 'react-bootstrap';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
 import Slider from '../components/Slider';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 
 import { listSliders } from '../actions/sliderActions';
+import { withRouter } from '../components/withRouter';
 
-const HomeScreen = () => {
-  const dispatch = useDispatch();
+class HomeScreen extends React.Component {
+  // eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
 
-  const sliderList = useSelector((state) => state.sliderList);
-  const { loading, error, sliders } = sliderList;
+  componentDidMount() {
+    this.props.listSliders();
+  }
 
-  // console.log(sliders);
+  render() {
+    const { loading, error, sliders } = this.props.sliderList;
+    return (
+      <>
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <>
+            <div className="btn_donation_fixed_mobile d-block d-sm-none">
+              <Button type="button" variant="dark" className="w-100">
+                Make A Donation
+              </Button>
+              <Button type="button" variant="dark" className="w-100">
+                Seek A Donation
+              </Button>
+            </div>
+            <div className="btn_donation_fixed_desktop d-none d-lg-block">
+              {/* <Image src={'/images/upright-hand.png'}></Image> */}
+              <Button type="button" variant="dark" className="w-100">
+                Make A Donation
+              </Button>
+              <br />
+              <Button type="button" variant="dark" className="w-100">
+                Seek A Donation
+              </Button>
+            </div>
 
-  useEffect(() => {
-    dispatch(listSliders());
-  }, [dispatch]);
+            <Slider sliders={sliders} />
+          </>
+        )}
+      </>
+    );
+  }
+}
 
-  return (
-    <>
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant="danger">{error}</Message>
-      ) : (
-        <>
-          <div className="btn_donation_fixed_mobile d-block d-sm-none">
-            <Button type="button" variant="dark" className="w-100">
-              Make A Donation
-            </Button>
-            <Button type="button" variant="dark" className="w-100">
-              Seek A Donation
-            </Button>
-          </div>
-          <div className="btn_donation_fixed_desktop d-none d-lg-block">
-            {/* <Image src={'/images/upright-hand.png'}></Image> */}
-            <Button type="button" variant="dark" className="w-100">
-              Make A Donation
-            </Button>
-            <br />
-            <Button type="button" variant="dark" className="w-100">
-              Seek A Donation
-            </Button>
-          </div>
-
-          <Slider sliders={sliders} />
-        </>
-      )}
-    </>
-  );
+const mapStateToProps = (state) => {
+  return {
+    sliderList: state.sliderList,
+  };
 };
 
-export default HomeScreen;
+export default withRouter(
+  connect(mapStateToProps, { listSliders })(HomeScreen)
+);
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,96 +1,140 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { Link, Navigate } from 'react-router-dom';
+import { connect } from 'react-redux';
 import FormContainer from '../components/FormContainer';
-import { Button, Form, Row, Col } from 'react-bootstrap';
+import { Button, Form, Row } from 'react-bootstrap';
 import { login } from '../actions/userActions';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
+import { withRouter } from '../components/withRouter';
 
-const LoginScreen = ({ history }) => {
-  const [phone, setPhone] = useState('');
-  const [password, setPassword] = useState('');
+class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phone: '',
+      password: '',
+      enable: '',
+      redirect: '',
+    };
+  }
 
-  const dispatch = useDispatch();
-  const location = useLocation();
+  setInputValue(property, val) {
+    this.setState({
+      [property]: val,
+    });
+  }
 
-  const userLogin = useSelector((state) => state.userLogin);
-  const { loading, error, userInfo } = userLogin;
-  // console.log('Just ', userInfo);
-
-  const redirect = location.search ? location.search.split('=')[1] : '/';
-
-  useEffect(() => {
-    if (userInfo) {
-      history.push(redirect);
+  checkEnter(thisEvent) {
+    if (thisEvent.keyCode === 13) {
+      this.props.login(this.state.phone, this.state.password);
     }
-  }, [userInfo, history, redirect]);
+  }
 
-  const submitHandler = (e) => {
+  resetForm() {
+    this.setState({
+      phone: '',
+      password: '',
+    });
+  }
+
+  submitHandler = (e) => {
     e.preventDefault();
-    dispatch(login(phone, password));
+    this.props.login(this.state.phone, this.state.password);
+    this.resetForm();
   };
 
-  return (
-    <FormContainer>
-      <div className="auth_component">
-        {error && <Message variant={'danger'}>{error}</Message>}
-        {loading && <Loader />}
-        <Form onSubmit={submitHandler}>
-          <Form.Group controlId="phone" className="form_field">
-            <span className="form_icon">
-              <i class="fa-solid fa-phone-flip"></i>
-            </span>
-            <Form.Control
-              type=""
-              placeholder="Enter phone number"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              required
-            ></Form.Control>
-          </Form.Group>
-          <Form.Group controlId="password" className="form_field">
-            <span className="form_icon">
-              <i class="fa-solid fa-key"></i>
-            </span>
-            <Form.Control
-              type="password"
-              placeholder="Enter password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            ></Form.Control>
-          </Form.Group>
+  render() {
+    const redirect = this.props.location.search
+      ? this.props.location.search.split('=')[1]
+      : '/';
 
-          <Button
-            type="submit"
-            variant="info"
-            className="w-100 form_submit_button"
+    const { loading, error, userInfo } = this.props.userLogin;
+
+    if (userInfo) {
+      return <Navigate to={redirect} />;
+    }
+
+    return (
+      <FormContainer>
+        <div className="auth_component">
+          {error && <Message variant={'danger'}>{error}</Message>}
+          {loading && <Loader />}
+          <Form onSubmit={this.submitHandler}>
+            <Form.Group controlId="phone" className="form_field">
+              <span className="form_icon">
+                <i className="fa-solid fa-phone-flip"></i>
+              </span>
+              <Form.Control
+                type="number"
+                placeholder="Enter phone number"
+                value={this.state.phone}
+                onChange={(e) => this.setInputValue('phone', e.target.value)}
+                required
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="password" className="form_field">
+              <span className="form_icon">
+                <i className="fa-solid fa-key"></i>
+              </span>
+              <Form.Control
+                type="password"
+                placeholder="Enter password"
+                value={this.state.password}
+                onChange={(e) => this.setInputValue('password', e.target.value)}
+                onKeyDown={(e) => this.checkEnter(e)}
+                required
+              ></Form.Control>
+            </Form.Group>
+
+            <Button
+              type="submit"
+              variant="info"
+              className="w-100 form_submit_button"
+              // onClick={() => this.submitHandler()}
+            >
+              Log In
+            </Button>
+          </Form>
+          <Row>
+            <Link
+              to={
+                this.state.redirect
+                  ? `/register?redirect=${this.state.redirect}`
+                  : '/register'
+              }
+            >
+              <span className="common_link_hover">Forgotten password?</span>
+            </Link>
+          </Row>
+        </div>
+
+        <Row className="text-center">
+          <Link
+            to={
+              this.state.redirect
+                ? `/register?redirect=${this.state.redirect}`
+                : '/register'
+            }
           >
-            Log In
-          </Button>
-        </Form>
-        <Row>
-          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-            <span className="common_link_hover">Forgotten password?</span>
+            <Button
+              type="submit"
+              variant="success"
+              className="new_account_button"
+            >
+              Create New Account
+            </Button>
           </Link>
         </Row>
-      </div>
+      </FormContainer>
+    );
+  }
+}
 
-      <Row className="text-center">
-        <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-          <Button
-            type="submit"
-            variant="success"
-            className="new_account_button"
-          >
-            Create New Account
-          </Button>
-        </Link>
-      </Row>
-    </FormContainer>
-  );
+const mapStateToProps = (state) => {
+  return {
+    userLogin: state.userLogin,
+  };
 };
 
-export default LoginScreen;
+export default withRouter(connect(mapStateToProps, { login })(LoginScreen));
