c35009607f7fde7106a234716ca93bfd32c1e013
kucse160235 <afnan1635@cseku.ac.bd>
2022-06-18 23:38:36 +0600

    Addition of Sympathy Logo

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -5,11 +5,41 @@ import { Card, Row, Col, ProgressBar } from "react-bootstrap";
 class Post extends React.Component {
     constructor(props){
         super(props);  
+
+        this.state = {
+            sympathyIcon: '/images/transparent 1-01.png',
+            isChecked:false
+        };
+
+        this.unchekedSympqathyIcon = '/images/transparent 1-01.png';
+        this.chekedSympqathyIcon = '/images/transparent 2-01-01.png';
     }
+
+    setInputValue(property, val) {
+        this.setState({
+          [property]: val,
+        });
+      }
+
+      
+    toggleSympathyIcon= (isChecked)=>{
+        
+        if(isChecked === true){
+            // console.log("Checked")
+            this.setInputValue("sympathyIcon", this.chekedSympqathyIcon)
+            
+        }else{
+            this.setInputValue("sympathyIcon", this.unchekedSympqathyIcon)
+        }
+        this.setInputValue("isChecked", !this.state.isChecked)
+    }
+
+
 
     render(){
         const posts = this.props.posts;
-        
+    
+    
         return (
             <>
             {posts && posts.length !== 0 && 
@@ -34,8 +64,14 @@ class Post extends React.Component {
                         <Col md={2}>
                             <Card>100 views</Card>
                         </Col>
-                        <Col md={9}>
+                        <Col md={6}>
                             <ProgressBar now={post.collectedPercentage} label={`Collected ${post.collectedPercentage}%`} />
+                        </Col>
+                        <Col md={4}>
+                            
+                            <img src={this.state.sympathyIcon} width="200px" onClick={() => {this.toggleSympathyIcon(this.state.isChecked)}}></img>
+     
+                            
                         </Col>
                     </Row>
                 </Card>)
